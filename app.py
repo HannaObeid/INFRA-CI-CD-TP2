7342c4f87730995914203bdc0fe0a156103cbfc2
hanna <hanna.obeid@student-cs.fr>
2024-02-06 16:31:51 +0100

    Add Flask RESTful API for managing items

---
+++ app.py
@@ -0,0 +1,44 @@
+import random
+from flask import Flask, request
+from flask_restful import Resource, Api
+
+app = Flask(__name__)
+api = Api(app)
+
+# A simple in-memory structure to store items
+items = [{"name": f"item_{i}", "price": round(random.uniform(1.0, 100.0), 2)} for i in range(1, 11)]
+
+
+class Item(Resource):
+    def get(self, name):
+        item = next(filter(lambda x: x['name'] == name, items), None)
+        return {'item': item}, 200 if item else 404
+
+    def post(self, name):
+        if next(filter(lambda x: x['name'] == name, items), None):
+            return {'message': f"An item with name '{name}' already exists."}, 400
+
+        data = request.get_json()
+        item = {'name': name, 'price': data['price']}
+        items.append(item)
+        return item, 201
+
+    def delete(self, name):
+        global items
+        items = list(filter(lambda x: x['name'] != name, items))
+        return {'message': 'Item deleted'}
+
+    def put(self, name):
+        data = request.get_json()
+        item = next(filter(lambda x: x['name'] == name, items), None)
+        if item is None:
+            item = {'name': name, 'price': data['price']}
+            items.append(item)
+        else:
+            item.update(data)
+        return item
+
+api.add_resource(Item, '/item/<string:name>')
+
+if __name__ == '__main__':
+    app.run(debug=True)
